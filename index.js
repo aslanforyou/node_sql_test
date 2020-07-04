8685acebcb985f4d47b529cf4ab6e678ca4876d5
aslanforyou <aslanforyou@mail.ru>
2020-07-04 10:52:10 +0300

    books table filling generator

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 const booksRouter = require('./routes/books');
 
 app.use('/books', booksRouter);
